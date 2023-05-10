e0b8e5dc4486f87b22e931e5ea74595f5884da9d
Cherch <b.cherchiglia@gmail.com>
2023-05-10 18:14:42 -0400

    JS Border Recognition & CSS Element Styling

--- script.js
+++ script.js
@@ -1,39 +1,105 @@
-//// ***CONSTANTS *** ////
+// OK HERE WE GO //
 
 
-// The PUCK //
+// CONSTANTS //
 
-const puck = document.getElementById('puck');
-const rect = puck.getBoundingClientRect();
+const DELAY = 20
+const XMAX = 638
+const YMAX = 395
+const XMIN = 17.3
+const YMIN = 17.3
+const PXMOVE = 2
 
-// The ICE //
-const ice = document.getElementById('ice');
+// STATE //
 
+let rightInterval
+let leftInterval
+let upInterval
+let downInterval
 
+// CACHED ELEMENTS //
 
-
-//// ***STATE*** ////
-
-
-
-
-
-
-
-//// ***CACHED ELEMENTS*** ////
-
-
-
-
-
-
-//// ***EVENT LISTENERS*** ////
+const stick = document.querySelector(".stick");
+const puck = document.querySelector(".puck");
+const startButton = document.querySelector("#start");
+const container = document.querySelector(".ice");
+let contRect = container.getBoundingClientRect();
+let padRect = stick.getBoundingClientRect();
+console.log(contRect)
 
 
+// EVENT LISTENERS //
 
+const moveStick = (event) => {
+  let x = event.clientX;
+  let y = event.clientY;
+  stick.style.left = `${x - 20}px`;
+  stick.style.top = `${y - 20}px`;
+}
 
+// FUNCTIONS //
 
-//// ***FUNCTIONS*** ////
+const movePuckDown = () => {
+  clearInterval(upInterval);
+  let y = puck.getBoundingClientRect().top;
+  if (y > YMAX) {
+    clearInterval(downInterval);
+    upInterval = setInterval(movePuckUp, DELAY)
+  }
+  puck.style.top = `${y + PXMOVE}px`;
+}
+const movePuckUp = () => {
+  clearInterval(downInterval);
+  let y = puck.getBoundingClientRect().top;
+  if (y < YMIN) {
+    clearInterval(upInterval);
+    downInterval = setInterval(movePuckDown, DELAY)
+  }
+  puck.style.top = `${y - PXMOVE}px`;
+}
+const movePuckLeft = () => {
+  clearInterval(rightInterval)
+  let x = puck.getBoundingClientRect().left;
+  if (x < XMIN) {
+    clearInterval(leftInterval)
+    rightInterval = setInterval(movePuckRight, DELAY);
+  }
+  puck.style.left = `${x - PXMOVE}px`;
+}
+const movePuckRight = () => {
+  clearInterval(leftInterval)
+  let x = puck.getBoundingClientRect().left;
+  if (x > XMAX) {
+    clearInterval(rightInterval)
+    leftInterval = setInterval(movePuckLeft, DELAY);
+  }
+  puck.style.left = `${x + PXMOVE}px`;
+}
 
+const puckMotion = () => {
+  let x = puck.getBoundingClientRect().left;
+  let y = puck.getBoundingClientRect().top;
 
+  if (x < XMIN) {
+    if (leftInterval) {
+      clearInterval(leftInterval)
+    }
+    rightInterval = setInterval(movePuckRight, DELAY);
+  }
 
+  if (y < YMIN) {
+    if (upInterval) {
+      clearInterval(upInterval)
+    }
+    downInterval = setInterval(movePuckDown, DELAY);
+  }
+}
+
+// puckMotion();
+const startGame = () => {
+  rightInterval = setInterval(movePuckRight, DELAY);
+  downInterval = setInterval(movePuckDown, DELAY);
+  document.addEventListener("mousemove", moveStick);
+  // puckMotion();
+}
+startButton.addEventListener("click", startGame);
