e2c447ac2cfd52a8d7c307db1788ecbb60979f3b
Cherch <b.cherchiglia@gmail.com>
2023-05-10 16:46:35 -0400

    git add .

---
+++ script.js
@@ -0,0 +1,39 @@
+//// ***CONSTANTS *** ////
+
+
+// The PUCK //
+
+const puck = document.getElementById('puck');
+const rect = puck.getBoundingClientRect();
+
+// The ICE //
+const ice = document.getElementById('ice');
+
+
+
+
+//// ***STATE*** ////
+
+
+
+
+
+
+
+//// ***CACHED ELEMENTS*** ////
+
+
+
+
+
+
+//// ***EVENT LISTENERS*** ////
+
+
+
+
+
+//// ***FUNCTIONS*** ////
+
+
+
